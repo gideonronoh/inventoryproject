1052d12a8e821647dc13974b838d05f1b16c6f88
Gideon Rono <145061874+gideonronoh@users.noreply.github.com>
2024-06-30 13:04:23 +0300

    djangouser

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+CATEGORY = (
+    ('Stationary','Stationary'),
+    ('Electronics','Electronics'),
+    ('Food','Food'),
+)
+
+class Product(models.Model):
+      name = models.CharField(max_length=100, null=True)
+      name = models.CharField(max_length=20, choices=CATEGORY, null=True)
+      quantity = models.PositiveIntegerField(null=True)
+
+      def _str_(self):
+        return f*{self.name}-{self.quantity}
